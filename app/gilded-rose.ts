9d6f36ebd28835438a911c4acac7c456059a9295
codeink <jeroen@codeink.be>
2025-09-14 09:55:52 +0200

    changed else if to else

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -32,7 +32,7 @@ export class GildedRose {
           if (item.sellIn >= 10) item.quality += 1;
           else if (item.sellIn >= 5) item.quality += 2;
           else if (item.sellIn >= 0) item.quality += 3;
-          else if (item.sellIn <= -1) item.quality = 0;
+          else item.quality = 0;
           break;
 
         case "Conjured Mana Cake":
